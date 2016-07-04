fa7471208429dab18f4f1d051e01e96d851f1744
Marcus Yu <marcusyuzc1992@gmail.com>
2016-07-04 15:20:23 +0800

    unfied all csv files into one

--- dxcrawler.py
+++ dxcrawler.py
@@ -1,65 +1,72 @@
-# -*- coding:utf-8 -*-
-import urllib
+
 import urllib2
 import lxml.html as HTML
 import pandas
 import requests
 
-#get HTML
-url = 'http://dxy.com/column/special/153/'
+#url of websites needed to got
+file_url_num = [226,224,153,218,210]
+
+# set up user agent
 user_agent = 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.112 Safari/537.36'
 headers = {'User-Agent': user_agent}
 
-request = urllib2.Request(url, headers=headers)
-response = urllib2.urlopen(request)
-
-# Analyze
-content = response.read()
-content = content.decode('utf-8')
-htmlSource = HTML.fromstring(content)
-
-itemurl = htmlSource.xpath('//div[@class="col-article-list col-article-list-search col-search"]/ul/li/dl/dt/a/@href')
-# itemcontent = htmlSource.xpath('//div[@class="col-article-list col-article-list-search col-search"]/ul/li/dl/dd/div/text()')
-# itemtheme = htmlSource.xpath('//div[@class="col-article-list col-article-list-search col-search"]/ul/li/dl/dd/div[@class="hd-tit"]/h4[@class="hd"]/a/text()')
-# author = htmlSource.xpath('//div[@class="col-article-list col-article-list-search col-search"]/ul/li/dl/dd/div[@class="hd-tit"]/div[@class="author"]/a/text()')
-
-# compensate incompleted url
-for i in range(len(itemurl)):
-	itemurl[i] = 'http://dxy.com' + itemurl[i] + '/'
-
+item_ = []
 totalTag = []
 totalAuthor = []
 totalContent = []
 totalTime = []
 totalTopic = []
-#爬取文档下的内容
-for singleUrl in itemurl:
-	request = urllib2.Request(singleUrl, headers=headers)
+
+for url in file_url_num:
+	url_ = 'http://dxy.com/column/special/' + str(url) + '/'
+	request = urllib2.Request(url_, headers=headers)
 	response = urllib2.urlopen(request)
+
+	#Analyze
 	content = response.read()
 	content = content.decode('utf-8')
 	htmlSource = HTML.fromstring(content)
-	#爬取作者
-	singleAuthor = htmlSource.xpath('//p[@class="info"]/span[@class = "author"]/a/text()')
-	totalAuthor.append(singleAuthor)
-	#爬取标签
-	singleTag =  htmlSource.xpath('//div[@class="mod-tag mt50 J-col-tag"]/a/text()')
-	totalTag.append(singleTag)
-	#爬取时间
-	singleTime = htmlSource.xpath('//p[@class="info"]/span[@class = "time"]/text()')
-	totalTime.append(singleTime)
-	#爬取主题
-	singleTopic = htmlSource.xpath('//div[@class="pg-article-hd"]/h1/text()')
-	totalTopic.append(singleTopic)
-	#爬取文章内容
-	singleContent = htmlSource.xpath('//div[@class="editor-body"]/p/text()|//div[@class="editor-body"]/div/text()|//div[@class="editor-body"]/p/font/font/text()')
-	totalContent.append(singleContent)
 
-item = [totalTopic,totalTime,totalAuthor,totalTag,totalContent]
-#矩阵转置
-item = map(list ,zip(*item))
+	itemurl = htmlSource.xpath('//div[@class="col-article-list col-article-list-search col-search"]/ul/li/dl/dt/a/@href')
 
-#output csv file
-paper = pandas.DataFrame(item)
-paper.to_csv("153.csv",encoding='GB18030',header=False,index=False,mode='a')
+
+	# compensate incompleted url
+	for i in range(len(itemurl)):
+		itemurl[i] = 'http://dxy.com' + itemurl[i] + '/'
+
+	#get attributes of each page
+	for singleUrl in itemurl:
+		request = urllib2.Request(singleUrl, headers=headers)
+		response = urllib2.urlopen(request)
+		content = response.read()
+		content = content.decode('utf-8')
+		htmlSource = HTML.fromstring(content)
+		#get the author
+		singleAuthor = htmlSource.xpath('//p[@class="info"]/span[@class = "author"]/a/text()')
+		totalAuthor.append(singleAuthor)
+		#get tags
+		singleTag =  htmlSource.xpath('//div[@class="mod-tag mt50 J-col-tag"]/a/text()')
+		totalTag.append(singleTag)
+		#get time
+		singleTime = htmlSource.xpath('//p[@class="info"]/span[@class = "time"]/text()')
+		totalTime.append(singleTime)
+		#get topics
+		singleTopic = htmlSource.xpath('//div[@class="pg-article-hd"]/h1/text()')
+		totalTopic.append(singleTopic)
+		#get contents
+		singleContent = htmlSource.xpath('//div[@class="editor-body"]/p/text()|//div[@class="editor-body"]/div/text()|//div[@class="editor-body"]/p/font/font/text()')
+		totalContent.append(singleContent)
+
+#matrix transpose
+item_ = [totalTopic,totalTime,totalAuthor,totalTag,totalContent]
+item_ = map(list,zip(*item_))
+	
+#output csv files
+
+paper = pandas.DataFrame(item_)
+paper.to_csv("dxy.csv",encoding = 'GB18030',header = False,index = False,mode = 'a')
+
+
+
 
