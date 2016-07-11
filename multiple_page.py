6a97d424eb9a4e1406fec352ef618438c68f1b46
Marcus Yu <marcusyuzc1992@gmail.com>
2016-07-11 09:49:50 +0800

    fixed bugs and finally put them into use

--- multiple_page.py
+++ multiple_page.py
@@ -18,13 +18,13 @@ def getPage(url, encoding):
     return source
 
 # 写入csv文件中
-def WriteToCSV(csvName, text, zp):
-	if zp == 0:
+def WriteToCSV(csvName, text, temp, zp):
+	if temp % 100 == 1 and zp == 0:
 		title = [("症状名称","症状链接","症状简述","症状起因","可能疾病","诊断详述","对症药品","常见检查","相似症状")]
 		title = pandas.DataFrame(title)
-		title.to_csv(csvName, encoding='utf-8', header=False, index=False, mode='a')
+		title.to_csv(csvName, encoding='GB18030', header=False, index=False, mode='a')
 	paper = pandas.DataFrame(text)
-	paper.to_csv(csvName, encoding='utf-8', header=False, index=False, mode='a')
+	paper.to_csv(csvName, encoding='GB18030', header=False, index=False, mode='a')
 
 # 获取下一页的URL
 def getNextPageURL(source):
@@ -79,7 +79,6 @@ while urlTemp != None:
 	url = 'http://jbk.39.net' + urlTemp
 	print '-------------------------------------------'
 	print 'temp' + str(temp) + ':' + url
-	temp = temp + 1
 
 
 	#get the HTML source code of the current page
@@ -96,17 +95,18 @@ while urlTemp != None:
 		Singleurl = blbyUrl[zp]
 		info = getInfo(title,Singleurl)
 		if temp <= 100:
-			WriteToCSV('mainPage\mainURL_001-100.csv', info ,zp)
+			WriteToCSV('ZhengZhuang_001-100.csv', info ,temp,zp)
 		elif temp <= 200 and temp > 100:
-			WriteToCSV('mainPage\mainURL_101-200.csv', info ,zp)
+			WriteToCSV('ZhengZhuang_101-200.csv', info ,temp,zp)
 		elif temp <= 300 and temp > 200:
-			WriteToCSV('mainPage\mainURL_201-300.csv', info ,zp)
+			WriteToCSV('ZhengZhuang_201-300.csv', info ,temp,zp)
 		elif temp <= 400 and temp > 300:
-			WriteToCSV('mainPage\mainURL_301-400.csv', info ,zp)
+			WriteToCSV('ZhengZhuang_301-400.csv', info ,temp,zp)
 		elif temp <= 500 and temp > 400:
-			WriteToCSV('mainPage\mainURL_401-500.csv', info ,zp)
+			WriteToCSV('ZhengZhuang_401-500.csv', info ,temp,zp)
 		elif temp <= 600 and temp > 500:
-			WriteToCSV('mainPage\mainURL_501-600.csv', info ,zp)
+			WriteToCSV('ZhengZhuang_501-600.csv', info ,temp,zp)
 		else:
-			 WriteToCSV('mainPage\mainURL_601-700.csv',info ,zp)
+			 WriteToCSV('ZhengZhuang_601-700.csv', info ,temp,zp)
+	temp = temp + 1
 	urlTemp = getNextPageURL(source)
